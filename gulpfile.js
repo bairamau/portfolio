152699c6f33765aa87b87041f53f23dc5450c2df
Zaur Bairamau <bairamau.z@gmail.com>
2019-02-10 17:49:43 +0300

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,24 @@
-var gulp = require('gulp');
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const csso = require("gulp-csso");
+const autoprefixer = require("gulp-autoprefixer");
+
+function html() {
+  return gulp.src("app/index.html").pipe(gulp.dest("dist"));
+}
+
+function img() {
+  return gulp.src("app/images/*.*").pipe(gulp.dest("dist/images"));
+}
+
+function css() {
+  return gulp
+    .src("app/scss/index.scss")
+    .pipe(sass())
+    .pipe(autoprefixer({ browsers: ["last 99 versions"], cascade: false }))
+    .pipe(csso())
+    .pipe(gulp.dest("dist/css"));
+}
+
+exports.build = gulp.series(html, css, img);
